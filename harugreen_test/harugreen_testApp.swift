51d883d68700437c6d1344452de206a590b4108b
Morpheus <wsy04018@naver.com>
2023-08-14 00:49:12 +0900

    Initial Commit

---
+++ harugreen_test/harugreen_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  harugreen_testApp.swift
+//  harugreen_test
+//
+//  Created by WooSY on 2023/08/14.
+//
+
+import SwiftUI
+
+@main
+struct harugreen_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
